09f254c088182d003142339233a84d6a1ab07fbd
shixiao <shixiao.andy@gmail.com>
2018-03-25 15:58:53 +0800

    add user

---
+++ bubble/apps/user/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
+
+class UserProfile(AbstractUser):
+    choices_gender = (
+        ('male','男'),
+        ('demale','女')
+    )
+    nick_name = models.CharField(max_length=50, verbose_name='昵称', default='wow')
+    birthday = models.DateField(verbose_name='生日', null=True, blank=True)
+    gender = models.CharField(choices=choices_gender,default='male',max_length=10)
+    address = models.CharField(max_length=100, default='')
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    header_image = models.ImageField(upload_to='image/%Y/%m', default='')
+
+
+    class Meta:
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
